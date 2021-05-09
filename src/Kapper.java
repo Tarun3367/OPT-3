9eb32a3a6f20a2be6150d95738800f55abc40c40
Tarun <tarungulraj@gmail.com>
2021-05-09 17:53:11 +0200

    basisprijs berekenen werkt en is getest

--- src/Kapper.java
+++ src/Kapper.java
@@ -2,4 +2,10 @@ public class Kapper {
     public boolean checkOfAfspraakGemaaktKanWorden(boolean nietGesloten, boolean voldoendePlek, boolean geenPauze){
         return nietGesloten&&voldoendePlek&&geenPauze;
     }
+    public double berekenPrijs(Integer leeftijd){
+        if(leeftijd<10){return 10;}
+        if(leeftijd<18){return 15;}
+        return 17.50;
+    }
+
 }
--- test/KapperTest.java
+++ test/KapperTest.java
@@ -1,13 +1,26 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class KapperTest {
-
+    Kapper kapper=new Kapper();
     @org.junit.jupiter.api.Test
     void checkOfAfspraakGemaaktKanWorden() {
-        Kapper kapper=new Kapper();
         assertTrue(kapper.checkOfAfspraakGemaaktKanWorden(true, true, true));
         assertFalse(kapper.checkOfAfspraakGemaaktKanWorden(false, true, true));
         assertFalse(kapper.checkOfAfspraakGemaaktKanWorden(true, false, true));
         assertFalse(kapper.checkOfAfspraakGemaaktKanWorden(true, true, false));
+    }
+
+    @Test
+    void berekenPrijs() {
+        assertEquals(10, kapper.berekenPrijs(0), 0.001);
+        assertEquals(10, kapper.berekenPrijs(1), 0.001);
+        assertEquals(10, kapper.berekenPrijs(9), 0.001);
+        assertEquals(15, kapper.berekenPrijs(10), 0.001);
+        assertEquals(15, kapper.berekenPrijs(11), 0.001);
+        assertEquals(15, kapper.berekenPrijs(17), 0.001);
+        assertEquals(17.50, kapper.berekenPrijs(18), 0.001);
+        assertEquals(17.50, kapper.berekenPrijs(19), 0.001);
     }
 }
